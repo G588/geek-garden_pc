ed84ecbc0d934fddd753cc4bc1e298e7f6f66d04
Gushilong <1466905783@qq.com>
2022-04-21 09:14:25 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"
+import App from "./App";
+// 引入样式
+import 'antd/dist/antd.css'
+import {DatePicker} from 'antd';
+
+// 渲染
+ReactDOM.render(<App/>, document.getElementById("root"))
